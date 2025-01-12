a5e8f9221ce0645492461c5f996a314978ba3583
bgd <trifan.bogdan09@yahoo.com>
2025-01-12 16:21:00 +0200

    functional 1

--- main.py
+++ main.py
@@ -1,57 +1,85 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-ani = [1972, 1973, 1974, 1975, 1976, 1977, 1978, 1979, 1980, 1981, 1982, 1983, 1984, 1985, 1986, 1987, 1988, 1989, 1990, 1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998, 1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023 ]
-rata_inflatie = [0.0, 0.7, 1.1, 0.2, 0.6, 0.6, 1.6, 2.0, 2.1, 3.1, 17.8, 4.1, 1.1, 0.8, 1.0, 0.9, 2.2, 1.1, 5.1, 170.2, 210.4, 256.1, 136.7, 32.3, 38.8, 154.8, 59.1, 45.8, 45.7, 34.5, 22.5, 15.3, 11.9, 9.0, 6.6, 4.8, 7.9, 5.6, 6.1, 5.8, 3.3, 4.0, 1.1, -0.6, -1.5, 1.3, 4.6, 3.8, 2.6, 5.1, 13.8, 10.4 ]
+PERIOADA_EXPERIMENT = 5 # 5 ani
 
-medie_inflatie = np.mean(rata_inflatie)
-deviatia_standard = np.std(rata_inflatie)
+class Inflation:
+    def __init__(self, ani, rata_inflatie, suma_economisita, numar_simulari = 20_000_000):
+        self.ani = ani
+        self.rata_inflatie = rata_inflatie
+        self.numar_simulari = numar_simulari
+        self.suma_economisita = suma_economisita
 
-suma_economisita = 100
-peste_ani = 5
-numar_simulari = 100_000
+        self.medie_inflatie = np.mean(self.rata_inflatie)
+        self.deviatia_standard = np.std(self.rata_inflatie)
+        print(f'Media inflatiei in Romania (2000 -> 2023): {round(self.medie_inflatie, 2)}%')
+        print(f'Deviatia standard a mediei (2000 -> 2023): ±{round(self.deviatia_standard, 2)}%')
 
-# pentru simularea inflatiei este folosita distributia normala (gaussiana)
-# vezi documentatie https://numpy.org/doc/2.1/reference/random/generated/numpy.random.normal.html
-# sau in cartea `Probability and Statistics for Computer Scientists` p.90
-# sau cursul de la facultate (8 sau 9)
-simulare_inflatie = np.random.normal(medie_inflatie, deviatia_standard, size = (numar_simulari, peste_ani))
-simulare_inflatie = np.clip(simulare_inflatie, 0, None)
+        # generez o matrice cu perioada_experiment coloane si numar_simulari linii
+        self.simulare_inflatie = np.random.normal(self.medie_inflatie, self.deviatia_standard, size=(numar_simulari, PERIOADA_EXPERIMENT))
 
-# vreau un array doar cu `suma_economisita`
-values = np.full([], suma_economisita, dtype=np.float64)
-for i in range(peste_ani):
-    # puterea de cumparare la timpul t este dat de: P(t) = suma_economisita / (1+i_1) * (1+i_2) * ... * (1 + i_n)
-    # i_n = rata inflatiei pentru anul n
-    values = values / (1 + simulare_inflatie[:, i] / 100)
+    def compute(self, nivel_de_incredere = 0.95, marja_eroare = 0.01):
+        # `suma_viitoare` = array de dimensiune `numar_simulari` cu valoarea `suma_economisita`
+        suma_viitoare = np.full(self.numar_simulari, self.suma_economisita, dtype = np.float64)
 
-medie_viitor = np.mean(values)
-deviatie_viitor = np.std(values)
 
-print(f'Peste {peste_ani} ani, {suma_economisita} lei vor insemna {round(medie_viitor, 2)} lei (± {round(deviatie_viitor, 2)} lei)')
+        for i in range(PERIOADA_EXPERIMENT): # se aplica inflatia fiecarui an
+            suma_viitoare = suma_viitoare / (1 + self.simulare_inflatie[:, i] / 100)
 
-plt.figure(figsize=(10, 6))
-plt.hist(values, bins=50, edgecolor='black', alpha=0.75, color = 'paleturquoise', label='Distribuția valorii economiilor')
-plt.axvline(medie_viitor, color='red', linestyle='dashed', linewidth=2, label=f'Media: {medie_viitor:.2f} lei')
-plt.axvline(medie_viitor - deviatie_viitor, color='midnightblue', linestyle='dotted', linewidth=2, label=f'-1σ: {medie_viitor - deviatie_viitor:.2f} lei')
-plt.axvline(medie_viitor + deviatie_viitor, color='midnightblue', linestyle='dotted', linewidth=2, label=f'+1σ: {medie_viitor + deviatie_viitor:.2f} lei')
-plt.xticks(np.arange(0, 101, 5))
-plt.title('Distribuția valorii viitoare a economiilor', fontsize=14)
-plt.xlabel('Valoarea reală (lei)', fontsize=12)
-plt.ylabel('Frecvența', fontsize=12)
-plt.legend()
-plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.tight_layout()
-plt.show()
+        medie_viitor = np.mean(suma_viitoare) # calculez media inflatiei peste cei 5 ani.
+        deviatia_standard_viitor = np.std(suma_viitoare) # calculez deviatia standard a inflatiei din urmatorii 5 ani
 
-plt.figure(figsize=(10, 6))
-plt.plot(ani, rata_inflatie, marker='o', linestyle='-', color='navy', label='Rata inflației (%)')
-plt.axhline(medie_inflatie, color='red', linestyle='dashed', linewidth=1.5, label=f'Media istorică: {medie_inflatie:.2f}%')
-plt.fill_between(ani, medie_inflatie - deviatia_standard, medie_inflatie + deviatia_standard, color='gray', alpha=0.2, label='±1σ')
-plt.title('Evoluția ratei inflației în timp', fontsize=14)
-plt.xlabel('Ani', fontsize=12)
-plt.ylabel('Rata inflației (%)', fontsize=12)
-plt.legend()
-plt.grid(axis='y', linestyle='--', alpha=0.7)
-plt.tight_layout()
-plt.show()
+        z = 1.65 # din z-table
+
+        numar_necesar_simulari = (z * deviatia_standard_viitor / (marja_eroare * medie_viitor)) ** 2
+
+        print(f'Peste {PERIOADA_EXPERIMENT} ani, {self.suma_economisita} lei vor insemna {round(medie_viitor, 2)} lei (± {round(deviatia_standard_viitor, 2)} lei)')
+        print(f'Pentru o eroare de ±{round(marja_eroare * 100, 2)}% cu nivelul de încredere {round(nivel_de_incredere * 100, 2)}%, sunt necesare {int(np.ceil(numar_necesar_simulari))} simulari')
+
+    def plot(self):
+        # Grafic 1: Evoluția ratei inflației în timp
+        lower_bound = self.medie_inflatie - self.deviatia_standard
+        upper_bound = self.medie_inflatie + self.deviatia_standard
+
+        plt.figure(figsize=(10, 6))
+        plt.plot(self.ani, self.rata_inflatie, marker='o', linestyle='-', color='navy', label='Rata inflației (%)')
+        plt.axhline(self.medie_inflatie, color='red', linestyle='dashed', linewidth=1.5,
+                    label=f'Media istorică: {round(self.medie_inflatie, 2)}%')
+        plt.fill_between(self.ani, lower_bound, upper_bound, color='gray', alpha=0.2,
+                         label=f'Deviatia σ: {round(self.deviatia_standard, 2)}%')
+        plt.title('Evoluția ratei inflației în timp', fontsize=14)
+        plt.xlabel('Ani', fontsize=10)
+        plt.ylabel('Rata inflației (%)', fontsize=12)
+        plt.legend()
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
+        plt.tight_layout()
+        plt.show()
+
+        # Grafic 2: Distribuția simulărilor inflației
+        medie_simulari = np.mean(self.simulare_inflatie)
+        std_simulari = np.std(self.simulare_inflatie)
+        lower_bound = medie_simulari - std_simulari
+        upper_bound = medie_simulari + std_simulari
+
+        plt.figure(figsize=(10, 6))
+        plt.hist(self.simulare_inflatie.flatten(), bins=100, color='skyblue', edgecolor='black', alpha=0.7,
+                 density=True)
+        plt.axvline(medie_simulari, color='red', linestyle='dashed', linewidth=1.5,
+                    label=f'Medie inflatie: {medie_simulari:.2f}%')
+        plt.axvline(lower_bound, color='orange', linestyle='dashed', linewidth=1.2, label=f'Derivatia (σ): {round(std_simulari, 2)}%')
+        plt.axvline(upper_bound, color='orange', linestyle='dashed', linewidth=1.2)
+        plt.title('Distribuția simulărilor inflației', fontsize=14)
+        plt.xlabel('Rata inflației simulate (%)', fontsize=10)
+        plt.ylabel('Densitate', fontsize=12)
+        plt.legend()
+        plt.grid(axis='y', linestyle='--', alpha=0.7)
+        plt.tight_layout()
+        plt.show()
+
+
+ani = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023]
+rata_inflatie = [45.7, 34.5, 22.5, 15.3, 11.9, 9.0, 6.6, 4.8, 7.9, 5.6, 6.1, 5.8, 3.3, 4.0, 1.1, -0.6, -1.5, 1.3, 4.6, 3.8, 2.6, 5.1, 13.8, 10.4]
+
+inflatie = Inflation(ani, rata_inflatie, 100, 2900)
+inflatie.compute()
+inflatie.plot()
